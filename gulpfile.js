f6aeb419d254af617289ec8811915d2cd743300f
Dennis Zhao <denniszhao@berkeley.edu>
2014-10-28 20:32:53 -0700

    Adds gulp as the front-end build system, minor restructuring of public/ directory, adds gulp dependencies

---
+++ gulpfile.js
@@ -0,0 +1,58 @@
+/**
+ *  Gulpfile for managing/building front-end services.
+ *
+ *  run `gulp watch` to watch for all changes in ./public/css and ./public/js
+ *
+ */
+
+// Gulp dependencies
+var gulp = require('gulp'),
+    browserify = require('browserify'),
+    uglify = require('gulp-uglify'),
+    source = require('vinyl-source-stream'),
+    sass = require('gulp-sass'),
+    gutil = require('gulp-util'),
+    minifycss = require('gulp-minify-css');
+
+function logErrorAndIgnore(err) {
+    console.log(err.toString());
+    this.emit('end');
+}
+
+// Path config to CSS/JS/etc.
+var paths = {
+    js: {
+        main: "./public/js/main.js",
+        source: "./public/js/**/*.js",
+        target: "./public/build/js/",
+    },
+    css: {
+        source: "./public/css/**/*.scss",
+        target: "./public/build/css/"
+    }
+};
+
+gulp.task('scripts', function () {
+    // Run browserify on the main file and send to the target destination
+    return browserify(paths.js.main)
+        .bundle()
+        .pipe(source('scripts.js'))
+        .on('error', logErrorAndIgnore)
+        // TODO: uglify for non dev
+        .pipe(gulp.dest(paths.js.target));
+});
+
+gulp.task('sass', function () {
+    return gulp.src(source)
+      .pipe(sass())
+      .on("error", logErrorAndIgnore)
+      .pipe(minifycss())
+      // pipe to build
+      .pipe(gulp.dest(paths.css.target));
+});
+
+gulp.task('watch', function() {
+    // When files change, update
+    gulp.watch(paths.js.source, ['scripts']);
+    gulp.watch(paths.css.source, ['sass']);
+});
