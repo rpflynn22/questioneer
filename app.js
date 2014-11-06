7687feb9ee434bdb95f3cf9cf73d40096a739af2
rpflynn22 <rpflynn22@gmail.com>
2014-11-05 20:57:56 -0800

    add some files for api

--- app.js
+++ app.js
@@ -23,7 +23,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api/users', users);
+app.use('/api/posts', posts);
+app.use('/api/categories', categories);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/posts.js
+++ routes/posts.js
@@ -1,12 +1,8 @@
 var express = require('express');
 var router = express.Router();
-var mongoURI = process.env.QUESTIONEER_MONGO_URI;
-var mongoose = require('mongoose');
-mongoose.connect()
 
 /* GET users listing. */
 router.get('/', function(req, res) {
-  
   res.send('respond with a resource');
 });
 
