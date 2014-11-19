8ebc41c0e7477407ca4b546ef81feee91bb78c04
rpflynn22 <rpflynn22@gmail.com>
2014-11-18 20:45:52 -0800

    add an answer model, add title to post model. need to update Post model references in routes accordingly

--- models/PostModel.js
+++ models/PostModel.js
@@ -2,10 +2,12 @@ var mongoose = require('mongoose');
 var schema = mongoose.Schema;
 var db = mongoose.createConnection(process.env.QUESTIONEER_MONGO_URI);
 var ObjectId = schema.ObjectId;
+var validate = require('mongoose-validator').validate;
 
 var postSchema = mongoose.Schema({
   id: ObjectId,
-  user: {type: String, required: true, trim: true},
+  user: {type: ObjectId, ref: 'User'},
+  title: {type: String, trim: true, required: true, validate: validate('len', 0, 128)}
   postText: {type: String, trim: true},
   bounty: {type: Number},
   date: {type: Date}
