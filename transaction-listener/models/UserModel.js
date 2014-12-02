db6892044da0fd31435860d1625013fd7e485e94
Dennis Zhao <denniszhao@berkeley.edu>
2014-12-01 19:35:53 -0800

    user model

--- transaction-listener/models/UserModel.js
+++ transaction-listener/models/UserModel.js
@@ -1,18 +1,19 @@
 var mongoose = require('mongoose');
-var Schema = mongoose.Schema;
-var db = mongoose.createConnection(process.env.QUESTIONEER_MONGO_URI);
-var ObjectId = Schema.ObjectId;
+var schema = mongoose.Schema;
+var ObjectId = schema.ObjectId;
 
-var userSchema = mongoose.Schema({
+var userSchema = schema({
   id: ObjectId,
   userName: {type: String, required: true, trim: true, index: {unique: true}},
   password: {type: String, required: true, trim: true},
   email: {type: String, required: true, trim: true, index: {unique: true}},
   rippleAddress: {type: String, required: true, trim: true},
-  accountCredit: Number,
+  accountCredit: {type: Number, required: true},
+  posts: [{type: ObjectId, ref: 'Post'}],
+  answers: [{type: ObjectId, ref: 'Answer'}],
   date: Date
 });
 
-var User = db.model('User', userSchema);
+var User = mongoose.model('User', userSchema);
 
 module.exports = User;
