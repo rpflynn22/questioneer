aeb8a6fce522c302b2318c5813336519404b7449
rpflynn22 <rpflynn22@gmail.com>
2014-12-01 18:46:43 -0800

    De/serializing user msg gone.

--- web/passport/init.js
+++ web/passport/init.js
@@ -4,14 +4,11 @@ var User = require('../models/UserModel');
 
 module.exports = function(passport) {
   passport.serializeUser(function(user, done) {
-  	console.log('serializing user: ')
-  	console.log(user);
   	done(null, user._id);
   });
 
   passport.deserializeUser(function(id, done) {
   	User.findById(id, function(err, user) {
-  	  console.log('deserializing user: ', user);
   	  done(err, user);
   	});
   });
